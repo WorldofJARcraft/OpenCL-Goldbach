c61fc14e25ed4952e777644de12b748f72fe4b8a
Eric Ackermann <eric.ackermann@student.hpi.de>
2022-09-11 14:38:40 +0200

    Improve performance of OpenCL sieve

--- OpenCLSieve.cpp
+++ OpenCLSieve.cpp
@@ -4,6 +4,7 @@
 #include "OpenCLSieve.h"
 
 #define CHECK_CALL(call) do{cl_int res = (call); if(res != CL_SUCCESS){ fprintf(stderr,"OpenCL call failed in line %u with code %u", __LINE__, -res); exit(1);}} while(0);
+#define WORK_QUEUE_SIZE 64
 
 static std::tuple<cl::Device,cl::Platform> get_device() {
     std::vector<cl::Platform> platforms;
@@ -76,7 +77,7 @@ void compact_results(std::vector<uint32_t>& buffer, std::vector<uint32_t>& resul
 
 }
 
-static std::tuple<std::vector<uint32_t>,uint32_t> run_sieve_kernel(cl::Device& device, cl::Platform& platform, const uint32_t maxNumber) {
+static std::tuple<std::vector<uint32_t>,uint32_t> run_sieve_kernel(cl::Device& device, cl::Platform& platform, const uint32_t max_number) {
     std::string src="#define __CL_ENABLE_EXCEPTIONS\n" 
         "void kernel prime_sieve(__global uint * primes, const uint N) {\n"
         "uint start_offset = 0;\n"
@@ -101,7 +102,7 @@ static std::tuple<std::vector<uint32_t>,uint32_t> run_sieve_kernel(cl::Device& d
     }
 
 
-    cl::Buffer memBuf(context, CL_MEM_READ_WRITE, maxNumber * sizeof(uint32_t));
+    cl::Buffer mem_buf(context, CL_MEM_READ_WRITE, max_number * sizeof(uint32_t));
     cl::Kernel kernel(program, "prime_sieve", &err);
 
     if (err != CL_SUCCESS) {
@@ -109,22 +110,22 @@ static std::tuple<std::vector<uint32_t>,uint32_t> run_sieve_kernel(cl::Device& d
         exit(1);
     }
 
-    CHECK_CALL(kernel.setArg(0, memBuf));
-    CHECK_CALL(kernel.setArg(1, maxNumber));
+    CHECK_CALL(kernel.setArg(0, mem_buf));
+    CHECK_CALL(kernel.setArg(1, max_number));
 
     cl::CommandQueue queue(context, device);
 
     cl::Event kernel_finished;
 
-    CHECK_CALL(queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(maxNumber - 2), cl::NullRange,nullptr,&kernel_finished));
+    CHECK_CALL(queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(max_number - 2), cl::NullRange,nullptr,&kernel_finished));
     
     CHECK_CALL(kernel_finished.wait());
 
-    std::vector<uint32_t> buffer(maxNumber);
-    CHECK_CALL(queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, buffer.size() * sizeof(uint32_t), buffer.data()));
+    std::vector<uint32_t> buffer(max_number);
+    CHECK_CALL(queue.enqueueReadBuffer(mem_buf, CL_TRUE, 0, buffer.size() * sizeof(uint32_t), buffer.data()));
     CHECK_CALL(queue.finish());
 
-    std::vector<uint32_t> results(maxNumber);
+    std::vector<uint32_t> results(max_number);
     uint32_t index = 0;
 
     compact_results(buffer, results, index);
@@ -149,21 +150,18 @@ static void mark_edge_cases(std::vector<uint32_t>& vector) {
     }
 }
 
-static int64_t run_square_kernel(cl::Device& device, cl::Platform& platform, const uint32_t maxNumber,  const std::vector<uint32_t>& primes) {
-    std::string src = "#define __CL_ENABLE_EXCEPTIONS\n"
-        "void kernel square_sieve(__global uint * primes, __global uint* outputs, const uint number_outputs) {\n"
-        "uint start_offset = 1;\n"
-        "uint end_offset = number_outputs;\n"
-        "const uint prime = primes[get_global_id(0)];\n"
-        "uint square_number = 1;\n"
-        "outputs[prime] = 1;\n"
-        "uint result;\n"
-        "do{\n"
-            "result  = prime + 2 * square_number * square_number;\n"
-            "square_number ++;\n"
-            "outputs[result] = 1;\n"
-        "} while(result < number_outputs);\n"
-        "}\n";
+static void extend_primes(std::vector<uint32_t>& primes) {
+    const size_t old_size = primes.size();
+    const size_t new_size = old_size + WORK_QUEUE_SIZE - (old_size % WORK_QUEUE_SIZE);
+    // extend with 2 (which is a valid prime) so the kernel does not have to deal with the edge case
+    for (size_t current = 0; current < new_size - old_size; current++) {
+        primes.push_back(2);
+    }
+}
+
+static int64_t run_square_kernel(cl::Device& device, cl::Platform& platform, const uint32_t max_number,  const std::vector<uint32_t>& primes, const std::string& src) {
+    auto start = std::chrono::high_resolution_clock::now();
+    std::chrono::steady_clock::time_point end;
     std::vector<cl::Device> devices;
     cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1));
     platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
@@ -178,8 +176,8 @@ static int64_t run_square_kernel(cl::Device& device, cl::Platform& platform, con
         exit(1);
     }
 
-
-    cl::Buffer memBuf(context, CL_MEM_READ_WRITE, maxNumber * sizeof(uint32_t)), prime_buf(context, CL_MEM_READ_WRITE, primes.size() * sizeof(uint32_t));
+    // temporary buffer for the kernel to store marks in, 0-initialized
+    cl::Buffer mem_buf(context, CL_MEM_READ_WRITE, max_number * sizeof(uint32_t)), prime_buf(context, CL_MEM_READ_WRITE, primes.size() * sizeof(uint32_t));
     cl::Kernel kernel(program, "square_sieve", &err);
 
     if (err != CL_SUCCESS) {
@@ -188,8 +186,8 @@ static int64_t run_square_kernel(cl::Device& device, cl::Platform& platform, con
     }
 
     CHECK_CALL(kernel.setArg(0, prime_buf));
-    CHECK_CALL(kernel.setArg(1, memBuf));
-    CHECK_CALL(kernel.setArg(2, maxNumber));
+    CHECK_CALL(kernel.setArg(1, mem_buf));
+    CHECK_CALL(kernel.setArg(2, max_number));
 
 
     cl::CommandQueue queue(context, device);
@@ -198,41 +196,114 @@ static int64_t run_square_kernel(cl::Device& device, cl::Platform& platform, con
 
     CHECK_CALL(queue.enqueueWriteBuffer(prime_buf, CL_TRUE, 0, primes.size() * sizeof(uint32_t), primes.data()));
 
-    CHECK_CALL(queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(primes.size()), cl::NullRange, nullptr, &kernel_finished));
+    // one work item (i.e. GPU task) is responsible for one prime, iterating all factors
+    CHECK_CALL(queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(primes.size()), cl::NDRange(WORK_QUEUE_SIZE), nullptr, &kernel_finished));
 
     CHECK_CALL(kernel_finished.wait());
 
-    std::vector<uint32_t> buffer(maxNumber);
-    CHECK_CALL(queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, buffer.size() * sizeof(uint32_t), buffer.data()));
+    std::vector<uint32_t> buffer(max_number);
+    CHECK_CALL(queue.enqueueReadBuffer(mem_buf, CL_TRUE, 0, buffer.size() * sizeof(uint32_t), buffer.data()));
     CHECK_CALL(queue.finish());
 
     mark_edge_cases(buffer);
 
-    return get_smallest_non_marked(buffer);
+    int64_t result = get_smallest_non_marked(buffer);
+    end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Kernel invocation took " << std::chrono::duration<double, std::ratio<1>>(end - start).count() << " seconds" << std::endl;
+
+    return result;
 }
 
+static int64_t run_original_goldbach_kernel(cl::Device& device, cl::Platform& platform, const uint32_t maxNumber, const std::vector<uint32_t>& primes) {
+    const std::string src = "#define __CL_ENABLE_EXCEPTIONS\n"
+        "void kernel square_sieve(__global uint * primes, __global uint* outputs, const uint number_outputs) {\n"
+        "uint start_offset = 1;\n"
+        "uint end_offset = number_outputs;\n"
+        "const uint prime = primes[get_global_id(0)];\n"
+        "uint square_number = 1;\n"
+        "outputs[prime] = 1;\n"
+        "uint result;\n"
+        "do{\n"
+        "result  = prime + 2 * square_number * square_number;\n"
+        "square_number ++;\n"
+        "outputs[result] = 1;\n"
+        "} while(result < number_outputs);\n"
+        "}\n";
+    return run_square_kernel(device, platform, maxNumber, primes, src);
 
+}
 
+static int64_t run_variation_goldbach_kernel(cl::Device& device, cl::Platform& platform, const uint32_t maxNumber, const std::vector<uint32_t>& primes) {
+    const std::string src = "#define __CL_ENABLE_EXCEPTIONS\n"
+        "void kernel square_sieve(__global uint * primes, __global uint* outputs, const uint number_outputs) {\n"
+        "uint start_offset = 1;\n"
+        "uint end_offset = number_outputs;\n"
+        "const uint prime = primes[get_global_id(0)];\n"
+        "uint square_number_one = 1;\n"
+        "outputs[prime] = 1;\n"
+        "uint result;\n"
+        "while(square_number_one * square_number_one < number_outputs - prime){\n"
+            "uint square_number_two = 1;\n"
+            "do{\n"
+                "result  = prime + square_number_one * square_number_one + square_number_two * square_number_two;\n"
+                "square_number_two ++;\n"
+                "outputs[result] = 1;\n"
+            "} while(result < number_outputs);\n"
+            "square_number_one ++;\n"
+        "}\n"
+        "}\n";
+    return run_square_kernel(device, platform, maxNumber, primes, src);
+}
 
-int main()
+int main(int argc, const char *argv[])
 {
-    const int N = 10000;
-    int numberPrimes;
-    int64_t smallest_square;
+    const int default_n = 1000000;
+    int N = default_n;
+    int number_primes;
+    int64_t smallest_square, smallest_square_varied;
     std::vector<uint32_t> primes;
     std::vector<uint32_t> non_reachable_single_square;
 
     cl::Device device;
     cl::Platform platform;
-    std::tie(device,platform) = get_device();
-    std::tie(primes, numberPrimes) = run_sieve_kernel(device,platform,N);
 
-    std::cout << "Found " << numberPrimes << " primes below " << N << std::endl;
-    smallest_square = run_square_kernel(device, platform, N, primes);
+    if (argc > 1) {
+        N = std::atoi(argv[1]);
+        // 0 passed or conversion failed
+        if (!N) {
+            std::cerr << "Usage: " << argv[0] << "[ max_number, default " << default_n << "]\n";
+            return 1;
+        }
+    }
+    
+    // make sure that global work queue size is divisible by size of local work queue, saving edge cases
+    N += N % WORK_QUEUE_SIZE;
+
+    std::cout << "Limiting search to " << N << "\n";
+
+    std::tie(device,platform) = get_device();
+    std::tie(primes, number_primes) = run_sieve_kernel(device,platform,N);
+
+    extend_primes(primes);
+
+    std::cout << "Found " << number_primes << " primes below " << N << std::endl;
+    smallest_square = run_original_goldbach_kernel(device, platform, N, primes);
 
     std::cout << "Smallest number not representable by sum of a prime and double of a square number: ";
     if (smallest_square != -1) {
          std::cout << smallest_square;
+    }
+    else {
+        std::cout << "not found!";
+    }
+    std::cout << std::endl;
+
+    smallest_square_varied = run_variation_goldbach_kernel(device, platform, N, primes);
+
+    std::cout << "Smallest number not representable by sum of a prime and two square numbers: ";
+    if (smallest_square_varied != -1) {
+        std::cout << smallest_square_varied;
     }
     else {
         std::cout << "not found!";
--- OpenCLSieve.h
+++ OpenCLSieve.h
@@ -2,9 +2,15 @@
 
 #include <iostream>
 #include <fstream>
+
 // OpenCL includes
+#ifdef __APPLE__
+#include <OpenCL/cl.hpp>
+#else
 #include <CL/cl.hpp>
+#endif
+
 #include <cstdint>
 #include <tuple>
-
+#include <chrono>
 static void compact_results(std::vector<uint32_t>& buffer, std::vector<uint32_t>& results, uint32_t& index);
