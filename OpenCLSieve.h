a54714f1ce3f9f05c0a495719b45018acfb9631d
Eric Ackermann <eric.ackermann@student.hpi.de>
2022-09-11 13:31:07 +0200

    Initial commit - test Goldbach conjecture

---
+++ OpenCLSieve.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+// OpenCL includes
+#include <CL/cl.hpp>
+#include <cstdint>
+#include <tuple>
+
+static void compact_results(std::vector<uint32_t>& buffer, std::vector<uint32_t>& results, uint32_t& index);
